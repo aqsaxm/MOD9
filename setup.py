b59bd37378c8fadb93225c90846072e1ef4de185
aqsaxm <axsamajeed@gmail.com>
2023-10-25 12:54:57 -0400

    Completed

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'Demo',
+    version = '0.1',
+    description = 'A simple demonstration package',
+    url = 'https://github.com/aqsaxm/MOD9',
+    author = 'Aqsa Majeed', 
+    author_email = 'axsamajeed@gmail.com',
+    license = 'MIT', 
+    packages = ['demo']
+)
